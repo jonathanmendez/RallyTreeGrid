542fd8d3ac86da7afdd2c138693ef1457abf804d
Barry Mullan <mullan@gmail.com>
2013-09-17 16:50:02 -0400

    fixed release

--- App.js
+++ App.js
@@ -13,7 +13,8 @@
   
     var tagFilterFn = function (rec) { 
         console.log("tag Filter Fn",rec);
-        if (rec.data._type.toLowerCase() === "portfolioitem/feature") {
+        if (rec.data._type.toLowerCase() === "portfolioitem/theme") {
+        //if (rec.data._type.toLowerCase() === "portfolioitem/feature") {
         //if (rec.data._type.toLowerCase() === "portfolioitem/epic") {
             if ( filterTags.length > 0) {
                 for ( var x = 0 ; x < rec.data.Tags.length;x++) {
@@ -268,7 +269,14 @@
           "Name",
           "Priority",
           "PreliminaryEstimate",
-          "Release",
+        //   "Release",
+            {
+                dataIndex : 'Release',
+                text: 'Release',
+                renderer: function (value, metaData, record) {
+                    return value != undefined && value != null ? value._refObjectName : null;                    
+                }
+            },
            {
             xtype: 'templatecolumn',
             tpl: Ext.create('Rally.ui.renderer.template.PercentDoneByStoryPlanEstimateTemplate2'),
